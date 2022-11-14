16647080628bc470b4ddb83d6b46038e03d3127a
danny <danielolvera311@gmail.com>
2022-11-13 19:24:41 -0600

    Added comments and changed line with connect function in dbtest.js, added semicolons after each statement

--- BE/dbtest.js
+++ BE/dbtest.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
 const creds = require('./creds.json');
+
+// Connection details provided by creds.json
 const connection = mysql.createConnection(creds);
 
-connection.connect((err) => {
-    if(err) {
-        console.error(err);
-        return;
-    }
-});
+// Connect to the database
+connection.connect();
 
+// Run query and display results
 connection.query('SELECT * FROM users;', (error, results, fields) => {
     if(error) throw error;
     console.table(results);
-})
+});
 
+// Close connection when finished
 connection.end();
