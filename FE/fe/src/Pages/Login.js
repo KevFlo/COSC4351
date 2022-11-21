06f1915414c8a840727fbb731d4027b9e9dbc555
Kevin Flores <floreskevin51398@gmail.com>
2022-11-21 05:57:25 -0600

    Login/Register/Reservation skeleton code -Kevin

--- FE/fe/src/Pages/Login.js
+++ FE/fe/src/Pages/Login.js
@@ -1,89 +1,89 @@
-// // login page that connects to the backend
-// import React, { useState } from 'react';
-// import { Link, Redirect } from 'react-router-dom';
+// login page that connects to the backend
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 // import axios from 'axios';
 // import './Login.css';
 
 
-export default function Login() {
-    return (
-      <h1> Login</h1>
-
-  );
-}
-
-// const Login = () => {
-//     const [formData, setFormData] = useState({
-//         email: '',
-//         password: '',
-//     });
-    
-//     const { email, password } = formData;
-    
-//     const onChange = (e) =>
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-    
-//     const onSubmit = async (e) => {
-//         e.preventDefault();
-//         console.log('SUCCESS');
-//         const newUser = {
-//         email,
-//         password,
-//         };
-    
-//         try {
-//         const config = {
-//             headers: {
-//             'Content-Type': 'application/json',
-//             },
-//         };
-    
-//         const body = JSON.stringify(newUser);
-    
-//         const res = await axios.post('/api/users', body, config);
-//         console.log(res.data);
-//         } catch (err) {
-//         console.error(err.response.data);
-//         }
-//     };
-
+// export default function Login() {
 //     return (
-//         <div className="login">
-//             <div className="login-container">
-//                 <div className="login-title">Login</div>
-//                 <form className="login-form" onSubmit={(e) => onSubmit(e)}>
-//                     <div className="login-form-group">
-//                         <label className="login-form-label">Email</label>
-//                         <input
-//                             className="login-form-input"
-//                             type="email"
-//                             placeholder="Email Address"
-//                             name="email"
-//                             value={email}
-//                             onChange={(e) => onChange(e)}
-//                             required
-//                         />
-//                     </div>
-//                     <div className="login-form-group">
-//                         <label className="login-form-label">Password</label>
-//                         <input
-//                             className="login-form-input"
-//                             type="password"
-//                             placeholder="Password"
-//                             name="password"
-//                             value={password}
-//                             onChange={(e) => onChange(e)}
-//                             minLength="6"
-//                         />
-//                     </div>
-//                     <input type="submit" className="login-form-button" value="Login" />
-//                 </form>
-//                 <p className="login-Login">
-//                     Don't have an account? <Link to="/Login">Login</Link>
-//                 </p>
-//             </div>
-//         </div>
-//     );
-// };
+//       <h1> Login</h1>
 
-// export default Login;
+//   );
+// }
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    
+    const { email, password } = formData;
+    
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('SUCCESS');
+        const newUser = {
+        email,
+        password,
+        };
+    
+        try {
+        const config = {
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        };
+    
+        const body = JSON.stringify(newUser);
+    
+        // const res = await axios.post('/api/users', body, config);
+        // console.log(res.data);
+        } catch (err) {
+        console.error(err.response.data);
+        }
+    };
+
+    return (
+        <div className="login">
+            <div className="login-container">
+                <div className="login-title">Login</div>
+                <form className="login-form" onSubmit={(e) => onSubmit(e)}>
+                    <div className="login-form-group">
+                        <label className="login-form-label">Email</label>
+                        <input
+                            className="login-form-input"
+                            type="email"
+                            placeholder="Email Address"
+                            name="email"
+                            value={email}
+                            onChange={(e) => onChange(e)}
+                            required
+                        />
+                    </div>
+                    <div className="login-form-group">
+                        <label className="login-form-label">Password</label>
+                        <input
+                            className="login-form-input"
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => onChange(e)}
+                            minLength="6"
+                        />
+                    </div>
+                    <input type="submit" className="login-form-button" value="Login" />
+                </form>
+                <p className="login-Login">
+                    Don't have an account? <Link to="/Login">Login</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- FE/fe/src/Pages/Reservations.js
+++ FE/fe/src/Pages/Reservations.js
@@ -1,6 +1,124 @@
-export default function Reservation() {
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+
+const Reservation = () =>   {
+    const [formData, setFormData] = useState({
+        partySize: '',
+        date: '',
+        time: '',
+        name: '',
+        email: '',
+        phoneNumber: '',
+    });
+
+    const { partySize, date, time, name, email, phoneNumber } = formData;
+
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const newReservation = {
+            partySize,
+            date,
+            time,
+            name,
+            email,
+            phoneNumber,
+        };
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+
+            const body = JSON.stringify(newReservation);
+
+            // const res = await axios.post('/api/users', body, config);
+            // console.log(res.data);
+        } catch (err) {
+            console.error(err.response.data);
+        }
+    };
+
+
+
     return (
-      <h1> Reservation</h1>
+    //   <h1> Reservation</h1>
+        <div className="Reservation-Form">
+            <div className="Reservation-Form-Content">
+                <div className="Reservation-Form-Title">Reservation</div>
+                <form className="Reservation-Form-Form">
+                    <div className="Reservation-Form-Form-Group">
+                        <label className="Reservation-Form-Form-Label">Party Size</label>
+                        <input
+                            className="Reservation-Form-Form-Input"
+                            type="number"
+                            placeholder="Party Size"
+                            name="partySize"
+                            value={partySize}
+                            onChange={(e) => onChange(e)}
+                            required
+                        />
+                    </div>
+                    <div className="Reservation-Form-Form-Group">
+                        <label className="Reservation-Form-Form-Label">Date</label>
+                        <input
+
+                            className="Reservation-Form-Form-Input"
+                            type="date"
+                            placeholder="Date"
+                            name="date"
+                            value={date}
+                            onChange={(e) => onChange(e)}
+                            required
+                        />
+                    </div>
+                    <div className="Reservation-Form-Form-Group">
+                        <label className="Reservation-Form-Form-Label">Time</label>
+                        <input
+                            className="Reservation-Form-Form-Input"
+                            type="time"
+                            placeholder="Time"
+                            name="time"
+                            value={time}
+                            onChange={(e) => onChange(e)}
+                            required
+                            minLength="6"
+                        />
+                    </div>
+                    <div className="Reservation-Form-Form-Group">
+                        <label className="Reservation-Form-Form-Label">
+                            Phone Number
+                        </label>
+                        <input
+                            className="Reservation-Form-Form-Input"
+                            type="tel"
+                            placeholder="Phone Number"
+                            name="phoneNumber"
+                            value={phoneNumber}
+                            onChange={(e) => onChange(e)}
+                            required
+                            minLength="6"
+                        />
+                    </div>
+                    <input
+                        type="submit"
+                        className="Reservation-Form-Form-Button"
+                        value="Reserve"
+                    />
+                </form>
+                <p className="Reservation-Form-Text">
+                    Already have an account? <Link to="/login">Sign In</Link>
+                </p>
+            </div>
+        </div>
+        
 
   );
-}
+};
+
+export default Reservation;
