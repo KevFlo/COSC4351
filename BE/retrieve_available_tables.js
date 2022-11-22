7ab0813c9adad227253fb959bd832f5d322fec91
Daniel <danielolvera311@gmail.com>
2022-11-22 13:53:49 -0600

    Progress on setting table_numbers that accommodate the number_guests for mock data and being able to query tables avaialable for reservation

--- BE/retrieve_available_tables.js
+++ BE/retrieve_available_tables.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const creds = require('./creds.json');
+var date;
 
 // Connection details provided by creds.json
 const connection = mysql.createConnection(creds);
@@ -7,11 +8,11 @@ const connection = mysql.createConnection(creds);
 // Connect to the database
 connection.connect();
 
-// Run query and display results
-connection.query('SELECT * FROM users;', (error, results, fields) => {
+/*/ Run query and display results WIP
+connection.query(`SELECT table_number FROM reservations WHERE date BETWEEN '${date}' AND ADDTIME('${date}', '-02:00');`, (error, results, fields) => {
     if(error) throw error;
     console.table(results);
-});
+});*/
 
 // Close connection when finished
 connection.end();
