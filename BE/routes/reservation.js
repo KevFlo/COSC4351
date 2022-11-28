1ecb8dd2499b23f06b6d447ea1bcd00755e433fd
danny <danielolvera311@gmail.com>
2022-11-27 19:22:36 -0600

    Logging in now working

--- BE/routes/reservation.js
+++ BE/routes/reservation.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../db_client');
 
 router.get('/:partySize/:date/:time/:phoneNumber', (req, res) => {
     const {partySize, date, time, phoneNumber} = req.params;
