0d7774a83c053bb0f67416fb70fe0e510aa0c49f
Daniel <danielolvera311@gmail.com>
2022-11-29 15:47:03 -0600

    Can now track day of the week holidays except for memorial day

--- BE/test_files/high_traffic.js
+++ BE/test_files/high_traffic.js
@@ -1,15 +1,22 @@
-var date = new Date('2022-12-31');
+const date = new Date(2022, 11 - 1, 29);
+console.log(date.getDate());
+console.log(date.getMonth() + 1);
+console.log(date.getDay());
 const holidays = require('./date_holidays.json');
+// Formatted as "month-weekNumInMonth-dayOfTheWeek"
+const dotw_holidays = require('./dotw_holidays.json');
 
 function isWeekend(_date) {
     // Saturday and Sunday are considered day 6 and day 0 respectively
-    return _date.getDay() % 6 === 0;
+    return (_date.getDay()) % 6 === 0;
 }
 
 function isHoliday(_date) {
-    month_day = String(_date.getMonth() + 1)+'-'+String(_date.getDate() + 1);
-    return holidays.includes(month_day);
+    month_day = String(_date.getMonth() + 1)+'-'+String(_date.getDate());
+    month_week_dotw = String(_date.getMonth() + 1)+'-'+String(Math.ceil((_date.getDate()) / 7))+'-'+String(_date.getDay());
+    console.log(month_day);
+    console.log(month_week_dotw);
+    return holidays.includes(month_day) || dotw_holidays.includes(month_week_dotw);
 }
 
-console.log(isWeekend(date));
-console.log(isHoliday(date));
+console.log('High Traffic Day: '+isWeekend(date) || isHoliday(date));
