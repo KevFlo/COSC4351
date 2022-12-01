17a85d7d1a2b3928c857e2a6375d12055b065d04
Daniel <danielolvera311@gmail.com>
2022-12-01 10:40:20 -0600

    Fixed high traffic module not working for single digit months and/or days

--- BE/modules/high_traffic.js
+++ BE/modules/high_traffic.js
@@ -1,4 +1,4 @@
-const date = new Date('2022-06-04 15:20');
+const date = new Date('2015-01-01 10:20');
 const holidays = require('./date_holidays.json');
 // Formatted as "month-weekNumInMonth-dayOfTheWeek"
 const dotw_holidays = require('./dotw_holidays.json');
@@ -11,9 +11,9 @@ function isWeekend(_date) {
 function isHoliday(_date) {
     month_day = String(_date.getMonth() + 1)+'-'+String(_date.getDate());
     month_week_dotw = String(_date.getMonth() + 1)+'-'+String(Math.ceil((_date.getDate()) / 7))+'-'+String(_date.getDay());
-    //console.log(month_day);
-    //console.log(month_week_dotw);
-    return holidays.includes(month_day) || dotw_holidays.includes(month_week_dotw);
+    console.log(month_day);
+    console.log(month_week_dotw);
+    return (holidays.includes(month_day) || dotw_holidays.includes(month_week_dotw));
 }
 
 function isHighTraffic(_date) {
